4021b94de8a21f397038f6cd452994fb280e3239
Anya Lin <hukk1996@gmail.com>
2024-02-09 17:07:05 +0800

    y

--- common/config.sh
+++ common/config.sh
@@ -94,39 +94,27 @@ verifyProviders() {
 updateProvider() {
 	[ -x "$(command -v "$SINGBOX")" ] || { logs err "sing-box is not installed.\n"; return 1; }
 	local setting="$(cat "$MAINSET")"
-	isEmpty "$(jsonSelect setting '.providers')" && { logs err "updateProvider: No subscription available. Update failed.\n"; return 1; }
+	local providers="$(jsonSelect setting '.providers')"
+	verifyProviders "$providers" || return 1
 
-	local provider providers="$(jsonSelect setting '.providers')" count=0
-	local url tag ua filter
+	local provider provider_count="$(jsonSelect providers 'length')" count=0
+	local result UA FILTER
 
 	local time=$($DATE -u +%s%3N)
-	for i in $(seq 0 $[ $(jsonSelect providers 'length') -1 ]); do
+	for i in $(seq 0 $[ $provider_count -1 ]); do
 		provider="$(jsonSelect providers ".[$i]")"
-		[ "$(jsonSelect provider 'type')" = "object" ] || { logs warn "updateProvider: '$provider' is not a valid provider.\n"; continue; }
-		# Required
-		for k in url tag; do
-			eval "isEmpty \"\$(jsonSelect provider '.$k')\" && \
-				{ logs warn \"updateProvider: '\$provider' lost '$k' key.\n\"; continue; } || \
-				$k=\"\$(jsonSelect provider '.$k')\""
+		# Keys: url tag #subgroup #prefix ua filter
+		for k in url tag ua filter; do
+			eval "$k=\"\$(jsonSelect provider '.$k')\""
 		done
-		# Optional
-		for k in ua filter; do
-			eval "isEmpty \"\$(jsonSelect provider '.$k')\" || \
-				$k=\"\$(jsonSelect provider '.$k')\""
-		done
-		# Filter Verify
-		if isEmpty "$filter"; then
-			unset filter
-		else
-			filterVerify "$i" "$filter" || continue
-		fi
 		# Updating
-		local result UA="$ua" FILTER="$filter"
-		parse_subscription result "$url" "$filter" || continue
+		UA="$ua" FILTER="$filter"
+		parse_subscription result "$url" || continue
+
 		echo -n "$result" > "$SUBSDIR/$tag.json"
 		let count++
 	done
 	time=$[ $($DATE -u +%s%3N) - $time ]
-	logs yeah "Successfully updated $count providers of total $(jsonSelect providers 'length').\n"
+	logs yeah "Successfully updated $count providers of total $provider_count.\n"
 	logs yeah "Total time: $[ $time / 60000 ]m$[ $time / 1000 % 60 ]s$[ $time % 1000 ]ms.\n"
 }
